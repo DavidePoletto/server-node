f24ea2ea07f5e9812c5a6472cadecdf4f3b322f9
Davide Poletto <153924428+DavidePoletto@users.noreply.github.com>
2024-10-26 06:52:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Configura CORS per consentire tutte le origini temporaneamente
-app.use(cors()); 
+// Configura CORS per consentire tutte le origini
+app.use(cors());
 
 // Middleware per il parsing JSON
 app.use(express.json());
+
+// Forza l’intestazione CORS manualmente
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Consente tutte le origini
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Importa le rotte
 const routes = require('./routes');
@@ -22,4 +29,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
 
