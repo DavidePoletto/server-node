05534bae322172d135158c8c72000edafd3f1d07
Davide Poletto <153924428+DavidePoletto@users.noreply.github.com>
2024-10-26 08:10:03 +0200

    Update newsController.js

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -2,24 +2,28 @@ const axios = require('axios');
 
 exports.getGamingNews = async (req, res) => {
   try {
-    const page = req.query.page || 1; // Pagina corrente, default 1
+    const nextPage = req.query.nextPage || ''; // Prendi il valore di nextPage dalla richiesta, o usa una stringa vuota
     const response = await axios.get('https://newsdata.io/api/1/news', {
       params: {
-        apikey: 'pub_57344223f965b1a800b96279183cd37794130', // La tua chiave API
+        apikey: 'pub_57344223f965b1a800b96279183cd37794130',
         q: 'videogiochi',
         language: 'it',
         category: 'technology',
-        page, // Aggiungi il numero della pagina alla richiesta
-        page_size: 9, // Numero di articoli per pagina (puoi modificarlo)
+        page: nextPage,
       },
     });
 
-    res.json(response.data.results);
+    // Restituisci i dati e il parametro `nextPage`
+    res.json({
+      articles: response.data.results,
+      nextPage: response.data.nextPage,
+    });
   } catch (error) {
     console.error("Errore nella chiamata a Newsdata.io:", error.response ? error.response.data : error.message);
     res.status(500).json({ message: error.response ? error.response.data : error.message });
   }
 };
+
 
 
 
