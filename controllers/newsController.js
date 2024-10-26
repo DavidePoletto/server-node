0f00c1905eb564ed23234f834e9010fd43db2a2b
Davide Poletto <153924428+DavidePoletto@users.noreply.github.com>
2024-10-26 09:19:38 +0200

    Update newsController.js

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -2,32 +2,27 @@ const axios = require('axios');
 
 exports.getGamingNews = async (req, res) => {
   try {
-    const nextPage = req.query.nextPage || ''; // Usa `nextPage` se presente, altrimenti inizia dalla prima pagina
+    const nextPage = req.query.nextPage || ''; // Imposta pagina iniziale se `nextPage` è vuoto
     const params = {
       apikey: 'pub_57344223f965b1a800b96279183cd37794130',
       q: 'videogiochi',
       language: 'it',
       category: 'technology',
     };
-    if (nextPage) params.page = nextPage; // Usa `nextPage` se disponibile
+    if (nextPage) params.page = nextPage; // Aggiungi solo se presente
 
     const response = await axios.get('https://newsdata.io/api/1/news', { params });
-    console.log("Risposta completa dell'API Newsdata:", response.data);
-
-    // Verifica se `results` è un array
-    if (!Array.isArray(response.data.results)) {
-      throw new Error("Formato dei dati non corretto: 'results' non è un array");
-    }
 
     res.json({
       articles: response.data.results,
-      nextPage: response.data.nextPage, // Restituisci `nextPage` se disponibile
+      nextPage: response.data.nextPage, // Invia `nextPage` per il prossimo caricamento
     });
   } catch (error) {
     console.error("Errore nella chiamata a Newsdata.io:", error.response ? error.response.data : error.message);
     res.status(500).json({ message: error.response ? error.response.data : error.message });
   }
 };
+
 
 
 
