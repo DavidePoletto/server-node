c1fd966edd447723cace20ef6c21563c08dc47c8
Davide Poletto <153924428+DavidePoletto@users.noreply.github.com>
2024-11-04 06:56:54 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const newsController = require('../controllers/newsController');
-const shopController = require('../controllers/shopController')
+const shopController = require('../controllers/shopController');
 const { getGameDetails } = require('../controllers/getGameDetails');
 
+// Route senza '/api' poiché già aggiunto in server.js
 router.get('/news/gaming', newsController.getGamingNews);
 router.get('/shop/games', shopController.getShopGames);
-router.get('/api/shop/games/:gameId', getGameDetails);
+router.get('/shop/games/:gameId', getGameDetails);
 
 module.exports = router;
 
