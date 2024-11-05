d0707ecad6e95aaabcd6a02b106968e9720cc839
Davide Poletto <153924428+DavidePoletto@users.noreply.github.com>
2024-11-05 07:33:29 +0100

    Update getGameDetails.js

--- controllers/getGameDetails.js
+++ controllers/getGameDetails.js
@@ -6,12 +6,25 @@ exports.getGameDetails = async (req, res) => {
   const { gameId } = req.params;
 
   try {
-    const response = await axios.get(`https://api.rawg.io/api/games/${gameId}`, {
+    // Richiesta per i dettagli del gioco
+    const gameDetailsResponse = await axios.get(`https://api.rawg.io/api/games/${gameId}`, {
       params: { key: API_KEY },
     });
-    res.json(response.data); // Risponde con i dettagli del gioco
+
+    // Richiesta per gli screenshot del gioco
+    const screenshotsResponse = await axios.get(`https://api.rawg.io/api/games/${gameId}/screenshots`, {
+      params: { key: API_KEY },
+    });
+
+    // Combina i dettagli del gioco con gli screenshot
+    const gameDetails = gameDetailsResponse.data;
+    gameDetails.screenshots = screenshotsResponse.data.results.map(screenshot => screenshot.image);
+
+    // Risponde con i dettagli del gioco, inclusi gli screenshot
+    res.json(gameDetails);
   } catch (error) {
     console.error("Errore nel recupero dei dettagli del gioco:", error.response ? error.response.data : error.message);
     res.status(500).json({ message: error.response ? error.response.data : error.message });
   }
 };
+
