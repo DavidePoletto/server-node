7911b7d59ffd12220d924c30b1944967751b36ad
Davide Poletto <153924428+DavidePoletto@users.noreply.github.com>
2024-11-15 04:40:16 +0100

    hh

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
@@ -15,7 +15,8 @@ app.get('/', (req, res) => {
   res.send('Server is running!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
+
 
